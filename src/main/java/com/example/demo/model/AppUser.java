197fe0654889a735a79bb915b348eec2c8809a32
bala-amypo <bala@amypo.in>
2025-12-21 08:19:00 +0000

    User commit

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
 public class AppUser {
+
     private Long id;
     private String email;
     private String role;
+    private LocalDateTime createdAt;
 
     public AppUser() {}
 
@@ -11,9 +15,38 @@ public class AppUser {
         this.id = id;
         this.email = email;
         this.role = role;
+        this.createdAt = LocalDateTime.now();
     }
 
-    public Long getId() { return id; }
-    public String getEmail() { return email; }
-    public String getRole() { return role; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
