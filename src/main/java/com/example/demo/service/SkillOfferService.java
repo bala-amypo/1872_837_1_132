88d8c14fe6226799c890ce1876776b285f6c2cda
bala-amypo <bala@amypo.in>
2025-12-24 04:13:34 +0000

    User commit

--- src/main/java/com/example/demo/service/SkillOfferService.java
+++ src/main/java/com/example/demo/service/SkillOfferService.java
@@ -1,20 +1,15 @@
 package com.example.demo.service;
 
 import com.example.demo.model.SkillOffer;
-import java.util.Collections;
 import java.util.List;
 
-public class SkillOfferService {
+public interface SkillOfferService {
 
-    public SkillOffer createOffer(Object obj) {
-        return new SkillOffer();
-    }
+    SkillOffer createOffer(Object obj);
 
-    public SkillOffer getOfferById(long id) {
-        return new SkillOffer();
-    }
+    SkillOffer getOfferById(Long id);
 
-    public List<SkillOffer> getOffersByUser(long id) {
-        return Collections.emptyList();
-    }
+    List<SkillOffer> getAll();
+
+    List<SkillOffer> getOffersByUser(Long userId);
 }
