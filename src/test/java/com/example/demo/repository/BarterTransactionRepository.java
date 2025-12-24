2896a614d003acf69a93ac07b512b72c45dedbcd
bala-amypo <bala@amypo.in>
2025-12-24 18:58:15 +0000

    User commit

--- src/test/java/com/example/demo/repository/BarterTransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.BarterTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarterTransactionRepository
-        extends JpaRepository<BarterTransaction, Long> {
-}
--- src/test/java/com/example/demo/repository/SkillOfferRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.SkillOffer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillOfferRepository extends JpaRepository<SkillOffer, Long> {
-}
